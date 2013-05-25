9462dbdd1c85c3ecd4ca45be06851ae06cfdac8e
willh <william.hamill@gmail.com>
2013-05-25 11:47:55 +0100

    adding incident model, first pass

---
+++ model/incident.rb
@@ -0,0 +1,16 @@
+class Incident
+    
+    attr_accessor :summary, :date, :location, :category, :status
+
+    def initialize(summary, date, location, category, status)
+
+        @summary = summary
+        @detail = detail
+        @date = date
+        @location = location
+        @category = category
+        @status = status
+
+    end
+
+end
