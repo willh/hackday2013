d75e650f187b983518b558bacf06250fe0233f59
willh <william.hamill@gmail.com>
2013-05-25 15:34:19 +0100

    formatting in feedback class

--- model/feedback.rb
+++ model/feedback.rb
@@ -8,21 +8,21 @@ class feedback
      incident_comments, general_good_points, general_bad_points, general_comments,
      summary_severity, summary_safety, summary_priority, apologised, satisfied, would_recommend)
 
-    @location = location
-    @date = date
-    @time_of_day = time_of_day
-    @incident_type = incident_type
-    @incident_points = incident_points
-    @incident_comments = incident_comments
-    @general_good_points = general_good_points
-    @general_bad_points = general_bad_points
-    @general_comments = general_comments
-    @summary_severity = summary_severity
-    @summary_safety = summary_safety
-    @summary_priority = summary_priority
-    @apologised = apologised
-    @satisfied = satisfied
-    @would_recommend = would_recommend
+        @location = location
+        @date = date
+        @time_of_day = time_of_day
+        @incident_type = incident_type
+        @incident_points = incident_points
+        @incident_comments = incident_comments
+        @general_good_points = general_good_points
+        @general_bad_points = general_bad_points
+        @general_comments = general_comments
+        @summary_severity = summary_severity
+        @summary_safety = summary_safety
+        @summary_priority = summary_priority
+        @apologised = apologised
+        @satisfied = satisfied
+        @would_recommend = would_recommend
 
     end
 
