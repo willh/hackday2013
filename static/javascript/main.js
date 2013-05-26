0b0191273147dd3ba2a41d339e5cd3024c104ebb
Steven Alexander <steven.william.alexander@googlemail.com>
2013-05-26 13:04:51 +0100

    updated js to support multiple charts

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,9 +1,11 @@
 $( document ).ready(function() {
   console.log("ready!");
 
-  var chart = document.getElementById('treemap');
-  new best.charts.treemap.Widget(chart, {
-    title: 'Incidents by location',
-    jsonUrl: $(chart).attr('data-url')
-  }).draw(940, 400);
+  $(".treemap").each(function(index, node) {
+    new best.charts.treemap.Widget(node, {
+      title: 'Incidents by location',
+      jsonUrl: $(node).attr('data-url')
+    }).draw(940, 400);
+  });
+
 });
