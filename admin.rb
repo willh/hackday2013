19511fba92c90eebdad291859a55f6d0e2ae444d
Steven Alexander <steven.william.alexander@googlemail.com>
2013-05-26 11:18:28 +0100

    added treemap

--- admin.rb
+++ admin.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 get '/admin' do
   erb :"admin/index", :layout => :admin_layout
 end
@@ -8,4 +10,48 @@ end
 
 get '/admin/incident/:incident' do
   erb :"admin/incident", :layout => :admin_layout
+end
+
+get '/admin/api/incidents_by_location' do
+  {
+    :name => '/',
+    :value => 58,
+    :children => [
+      {
+        :name => 'Ward 10',
+        :value => 13,
+        :children => [
+          { :name => 'Facilities', :value => 2 },
+          { :name => 'Miscommunication', :value => 6 },
+          { :name => 'Drug Error', :value => 5 }
+        ]
+      },
+      {
+        :name => 'Reception',
+        :value => 23,
+        :children => [
+          { :name => 'Facilities', :value => 2 },
+          { :name => 'Miscommunication', :value => 21 }
+        ]
+      },
+      {
+        :name => 'Ward 6S',
+        :value => 18,
+        :children => [
+          { :name => 'Facilities', :value => 9 },
+          { :name => 'Miscommunication', :value => 6 },
+          { :name => 'Drug Error', :value => 3 }
+        ]
+      },
+      {
+        :name => 'Ward 21',
+        :value => 4,
+        :children => [
+          { :name => 'Facilities', :value => 1 },
+          { :name => 'Miscommunication', :value => 2 },
+          { :name => 'Drug Error', :value => 1 }
+        ]
+      },
+    ]
+  }.to_json
 end
---
+++ static/javascript/treemap.js
@@ -0,0 +1,91 @@
+var best = best || {};
+best.charts = best.charts || {};
+
+best.charts.treemap = best.charts.treemap || (function() {
+  var treemap_d3js = function treemap(_node, opts) {
+    this.opts = $.extend({}, best.charts.treemap.Widget.default_options, opts);
+    this.node = _node;
+  };
+
+  $.extend(treemap_d3js, {
+    chart_type: 'treemap',
+    default_options : {
+      title : '',
+      jsonUrl : '',
+      auto_resize : true
+    }
+  });
+
+  $.extend(treemap_d3js.prototype, {
+    _init : function() {
+      var that = this;
+
+      if (this.opts.auto_resize) {
+        $(window).on('resize', function() {
+          that._onWindowResize();
+        });
+      }
+    },
+
+    _onWindowResize : function() {
+      if ($(this.node).is(':visible') == false) {
+        return;
+      }
+
+      var that = this, jnode = $(this.node);
+
+      window.clearTimeout(this.timeout_id);
+      this.timeout_id = setTimeout(function() {
+        var width = jnode.innerWidth();
+        if (that.width != width) {
+          that.draw(width, that.height);
+        }
+      }, 50);
+    },
+
+    draw : function(w, h) {
+      var that = this;
+      console.log('draw treemap!');
+
+      var margin = {top: 40, right: 10, bottom: 10, left: 10},
+          width = w - margin.left - margin.right,
+          height = h - margin.top - margin.bottom;
+
+      var color = d3.scale.category20c();
+
+      var treemap = d3.layout.treemap()
+        .size([width, height])
+        .sticky(true)
+        .value(function(d) { return d.value; });
+
+      var div = d3.select("#treemap").append("div")
+          .style("position", "relative")
+          .style("width", (width + margin.left + margin.right) + "px")
+          .style("height", (height + margin.top + margin.bottom) + "px")
+          .style("left", margin.left + "px")
+          .style("top", margin.top + "px");
+
+      d3.json(that.opts.jsonUrl, function(error, root) {
+
+        var node = div.datum(root).selectAll(".node")
+          .data(treemap.nodes)
+          .enter().append("div")
+          .attr("class", "node")
+          .call(function() {
+            this.style("left", function(d) { return d.x + "px"; })
+                .style("top", function(d) { return d.y + "px"; })
+                .style("width", function(d) { return Math.max(0, d.dx - 1) + "px"; })
+                .style("height", function(d) { return Math.max(0, d.dy - 1) + "px"; });
+          })
+          .style("background", function(d) { return d.children ? color(d.name) : null; })
+          .text(function(d) { return d.children ? null : d.name + ' - ' + d.value; });
+
+        console.log('data!', root);
+      });
+    }
+  });
+
+  return {
+    Widget : treemap_d3js
+  };
+})();
